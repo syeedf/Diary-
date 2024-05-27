4cd6aa7c322ee293c6491368fd0acc82e1d57a0f
syeedf <syeed.farrakhan@bison.howard.edu>
2024-05-27 17:39:11 -0400

    Finished the basic login function for initial site.

--- server.js
+++ server.js
@@ -3,6 +3,17 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const knex = require('knex');
 
+const db = knex({
+    client: 'pg',
+    connection: {
+        host:'127.0.0.1',
+        user: 'postgres',
+        password: 'Ahmadhassan8',
+        database: 'logininmydiary'
+
+    } 
+})
+
 const app = express();
 
 let initialPath = path.join(__dirname, "public");
@@ -22,6 +33,47 @@ app.get('/register', (req, res)=>{
     res.sendFile(path.join(initialPath, "register.html"));
 })
 
+app.post('/register-user', (req,res)=>{
+    const {name, email, password } = req.body;
+     
+    if(!name.length || !email.length || !password.length){
+        res.json('fill all the fields');
+    }
+    else{
+        db("users").insert({
+            name: name, 
+            email: email, 
+            password: password
+        })
+    .returning(["name", "email"])
+    .then(data => {
+        res.json(data[0])
+    })
+    .catch(err => {
+        if(err.detail.includes('already exists')){
+            res.json('email already exists');
+        }
+    })
+    }
+})
+app.post('/login-user', (req, res) =>{
+    const { email, password } = req.body;
+
+    db.select('name', 'email')
+    .from('users')
+    .where({
+        email: email,
+        password: password
+    })
+    .then(data =>{
+        if(data.length){
+            res.json(data[0]);
+        }
+        else{
+            res.json('email or password is incorrect');
+        }
+    })
+})
 app.listen(3000, (req, res) => {
     console.log('listening on port 3000......');
 })
